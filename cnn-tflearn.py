b6e4fbe13a173a7d86403e1c10dcd4db2c59a3cb
Shadowalker1995 <henry664650770@gmail.com>
2021-04-10 03:58:27 -0400

    remove useless function

--- cnn-tflearn.py
+++ cnn-tflearn.py
@@ -97,19 +97,6 @@ def predict(input_array, targets, model, model_file=None, csv_file=None):
     return acc
 
 
-def variable_summaries(var):
-    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
-    with tf.name_scope('summaries'):
-        mean = tf.reduce_mean(var)
-        tf.summary.scalar('mean', mean)
-        with tf.name_scope('stddev'):
-            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-        tf.summary.scalar('stddev', stddev)
-        tf.summary.scalar('max', tf.reduce_max(var))
-        tf.summary.scalar('min', tf.reduce_min(var))
-        tf.summary.histogram('histogram', var)
-
-
 def main():
     # label_dict = gen_label()
     # print(label_dict)
--- train_val_split.py
+++ train_val_split.py
@@ -1,2 +1,2 @@
 import splitfolders
-splitfolders.ratio('all_images/', output="data_split", seed=1337, ratio=(0.8, 0.2))
+splitfolders.ratio('./data/8Classes-9041/all/', output="./data/8Classes-9041/split", seed=1337, ratio=(0.6, 0.2, 0.2))
